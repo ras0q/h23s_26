3f3134239acd081705c991d34c13a14e890a3c9c
ras0q <ras0q@users.noreply.github.com>
2023-06-17 09:23:12 +0900

    :sparkles: add endpoint comment

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -17,9 +17,31 @@ func New(repo *repository.Repository) *Handler {
 }
 
 func (h *Handler) SetupRoutes(api *echo.Group) {
-	// ping API
+	// Ping API
 	pingAPI := api.Group("/ping")
 	{
 		pingAPI.GET("", h.Ping)
 	}
+
+	// // User API
+	// userAPI := api.Group("/user")
+	// {
+	// 	userAPI.GET("/:userID", h.GetUser)
+	// 	userAPI.GET("/me", h.GetMe)
+	// 	userAPI.PATCH("/me/missions/:missionID", h.PatchMission)
+	// }
+
+	// // Mission api
+	// missionAPI := api.Group("/mission")
+	// {
+	// 	missionAPI.GET("", h.GetMissions)
+	// 	missionAPI.POST("", h.PostMission)
+	// 	missionAPI.GET("/:missionID", h.GetMission)
+	// }
+
+	// // Ranking API
+	// rankingAPI := api.Group("/ranking")
+	// {
+	// 	rankingAPI.GET("", h.GetRanking)
+	// }
 }
