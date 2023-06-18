04ce0109a37fd704b669af65a4c9958d911af773
Alter <tuuchan0420@gmail.com>
2023-06-18 09:25:29 +0900

    fix Ranking struct,query

--- internal/repository/ranking.go
+++ internal/repository/ranking.go
@@ -7,16 +7,17 @@ import (
 
 type (
 	Ranking struct {
-		Ranking []string `json:"ranking"`
+		Ranking []string
 	}
 )
 
 func (r *Repository) GetRanking(ctx context.Context) (*Ranking, error) {
 	ranking := Ranking{}
-	if err := r.db.SelectContext(ctx, &ranking.Ranking, "SELECT user_id FROM user_mission_relations GROUP BY user_id ORDER BY COUNT(*) DESC LIMIT 5"); err != nil {
+	ranking.Ranking = []string{}
+	if err := r.db.SelectContext(ctx, &ranking.Ranking, "SELECT user_id FROM user_mission_relations GROUP BY user_id ORDER BY COUNT(*) DESC"); err != nil {
 		return nil, fmt.Errorf("ranking: %w", err)
 	}
 
-	return &ranking,nil
+	return &ranking, nil
 
 }
